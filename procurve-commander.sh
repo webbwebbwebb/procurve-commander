404c2a677eb87fac4ae8145e4c99a8c33315037e
Mark Webb <mark.webb@rtgs.com>
2025-01-07 15:21:53 +0000

    Allow login on switches where password has not been set

--- procurve-commander.sh
+++ procurve-commander.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -e
 # procurve-commander.sh
-#
-# @author: Nestor Urquiza
-# @date: 20180316
-# @description: Sends the comma separated commands to an HP Procurve switch
-# @env: SWITCH_PASSWORD
+
+# @description: Send commands to an HP Procurve switch
+# @env: SWITCH_PASSWORD (optional)
 #
 
 USAGE="Usage: `basename $0` <user> <host> <commands (separated by comma)>"
@@ -15,10 +13,11 @@ then
     exit 1 
 fi
 
+passwordSupplied=true
 if [ -z "$SWITCH_PASSWORD" ]
 then
-    echo SWITCH_PASSWORD environment variable not set
-    exit 1
+    echo SWITCH_PASSWORD environment variable not set, will attempt passwordless login
+    passwordSupplied=false
 fi
 
 user=$1
@@ -28,7 +27,7 @@ commands=$3
 # set bash internal field separator to comma
 IFS=,
 
-# iterate througg commands to build expect commands
+# iterate through commands to build expect commands
 for command in $commands; do
   # trim
   command=`echo $command |  xargs`
@@ -39,8 +38,11 @@ done
 es="expect -c '"
 es="${es}  set timeout 20;"
 es="${es}  spawn ssh $user@$host;"
-es="${es}  expect \"password\";"
-es="${es}  send \"$SWITCH_PASSWORD\n\";"
+if [ "$passwordSupplied" = true ] 
+then
+  es="${es}  expect \"password\";"
+  es="${es}  send \"$SWITCH_PASSWORD\r\";"
+fi
 es="${es}  expect \"continue\";"
 es="${es}  send \"\n\";"
 es="${es}  $toBeSent"
